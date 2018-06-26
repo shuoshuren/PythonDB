be98ac54586407f12d23ecb80133410fd251cd7c
shuoshuren <534361124@qq.com>
2018-06-26 23:45:58 +0800

    mysql 更新删除语句

--- mysql/update.py
+++ mysql/update.py
@@ -4,25 +4,44 @@
 from pymysql import *
 
 """
-向mysql数据库中插入数据
+向mysql数据库中更改数据
 
 """
 
 
-def insert():
+def update():
+    """更新数据"""
     try:
         conn = connect(host='localhost', port=3306, db='python3', user='root', passwd='mysql', charset='utf8')
         cursor1 = conn.cursor()
 
-        sql = 'insert into students(name) values("郭小二")'
+        sql = 'update students set name = "王二小" where id = 8'
         cursor1.execute(sql)
         conn.commit()
 
         cursor1.close()
         conn.close()
+        print("ok")
+    except Exception:
+        print(Exception)
+
+
+def delete():
+    """删除数据"""
+    try:
+        conn = connect(host='localhost', port=3306, db='python3', user='root', passwd='mysql', charset='utf8')
+        cursor1 = conn.cursor()
+
+        sql = 'delete from students where id = 8'
+        cursor1.execute(sql)
+        conn.commit()
+
+        cursor1.close()
+        conn.close()
+        print("ok")
     except Exception:
         print(Exception)
 
 
 if __name__ == '__main__':
-    insert()
+    delete()
